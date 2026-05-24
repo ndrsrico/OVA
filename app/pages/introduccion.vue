545c3afbcf72fd9861f1a59a95c99df85874c008
Andrés Rico <andrescamiloricocerra@gmail.com>
2026-05-24 16:59:47 -0500

    Fix: Mejorar visualización de videos en iframe

--- app/pages/introduccion.vue
+++ app/pages/introduccion.vue
@@ -166,11 +166,12 @@
                     <iframe
                       :src="temaActual.video"
                       title="Video explicativo"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      referrerpolicy="strict-origin-when-cross-origin"
-                      allowfullscreen
-                      style="width: 100%; height: 100%; border: none;"
+                      width="100%"
+                      height="100%"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+                      allowfullscreen=""
+                      loading="lazy"
+                      style="border: none; position: absolute; top: 0; left: 0;"
                     ></iframe>
                   </v-responsive>
 
